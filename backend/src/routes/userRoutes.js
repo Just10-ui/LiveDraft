7c8af6ebebe01d80ebc5d98cd2305eb18348d3c3
Justin Apolong <apolongjustin49@gmail.com>
2026-01-16 16:42:36 +0800

    feat(userRoutes.js): Implement change password logic to an endpoint

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { userLogin, userSignup } from '../controller/userController.js';
+import { changePassword, userLogin, userSignup } from '../controller/userController.js';
 
 const userRoutes = Router();
 
 userRoutes.post('/signup', userSignup);
 userRoutes.post('/login', userLogin);
+userRoutes.put('/:userId', changePassword);
 
 export default userRoutes;
