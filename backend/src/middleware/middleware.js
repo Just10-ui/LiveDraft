2a01756c81003c879b5735d50aa00833a18e85bd
Justin <apolongjustin49@gmail.com>
2026-01-23 19:07:50 +0800

    feat(middleware.js): added token authentication

--- backend/src/middleware/middleware.js
+++ backend/src/middleware/middleware.js
@@ -1,5 +1,29 @@
 import cors from 'cors';
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const parseJson = express.json();
+
 export const crossOrig = cors({origin: 'http://127.0.0.1:5500'});
+
+export const verifyToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(400).json({message: 'No token provided'});
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({message: 'Invalid token'})
+  }
+};
